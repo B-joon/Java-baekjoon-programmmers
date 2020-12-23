6a2ea48efa56de8b7eac9e308adc4aab876e19c9
B-joon <dosel45@gmail.com>
2020-12-23 21:30:13 +0900

    String Prectice

---
+++ Java_prectice05/src/com/test/StringTest02.java
@@ -0,0 +1,34 @@
+package com.test;
+
+/*
+ * N개의 숫자가 공백 없이 쓰여있다.
+ * 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 첫재 줄에 숫자의 개수N(1 <= N <= 100)이 주어진다.
+ * 둘째 줄에 숫자 N개가 공백 없이 주어진다.
+ * 
+ * 출력
+ * 입력으로 주어진 숫자 N개의 합을 출력한다.
+ * 
+ */
+
+import java.util.Scanner;
+
+public class StringTest02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		String num = sc.next();
+		int sum = 0;
+		
+		for (int i = 0; i < n; i++) {
+			sum += num.charAt(i) - '0';
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+}
