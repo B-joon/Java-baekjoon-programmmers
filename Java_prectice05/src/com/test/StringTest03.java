4e74b2346893c861bdabd9ece7a488f2ee927cd5
B-joon <dosel45@gmail.com>
2020-12-25 20:16:00 +0900

    String Prectice

---
+++ Java_prectice05/src/com/test/StringTest03.java
@@ -0,0 +1,39 @@
+package com.test;
+
+/*
+ * 알파벳 소문자로만 이루어진 단어S가 주어진다. 각각의 알파벡에 대해서,
+ * 단어에 포함되어 있는경우에는 처음 등장하는 위치를, 호함되어 있지 않은 경우에는
+ * -1 을 출력하는 프로그램을 작성하시오
+ * 
+ * 입력
+ * 첫째 줄에 단어S가 주어진다. 단어의 길이는 100을 넘지 않으며 알파벳 소문자로만 이루어져 있다.
+ * 
+ * 출력
+ * 각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ...z가 처음 등장하는 위치를
+ * 공백으로 구분해서 출력한다. 만약, 어떤 알파벡이 단어에 포함되어 있지 않다면 -1을 출력한다.
+ * 단어의 첫 번째 글자는 0번째 위치이고, 두번째 글자는 1번째 위치이다.
+ * 
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringTest03 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String S = br.readLine();
+		
+		for (char i = 'a'; i <= 'z'; i++) {
+			if (i < 'z') {
+				System.out.print(S.indexOf(i) + " ");
+			} else {
+				System.out.print(S.indexOf(i));
+			}
+		}
+		
+	}
+}
